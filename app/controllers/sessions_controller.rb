0fb25173901d3e39d14c175ae02b9a9056a7cdf4
Gina Son <ginasunah33@gmail.com>
2017-08-06 22:37:28 -0400

    Add skeleton for logout link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
 class SessionsController < ApplicationController
   def new
+
   end
 
   def create
+    u = User.find_by(email: params[:session][:email])
+
+    if u && u.authenticate(params[:session][:password])
+      flash[:notice] = 'Sucessfully logged in!'
+      redirect_to root_url
+    else
+      flash.now[:alert] = 'Try again!'
+      render :new
+    end
   end
 
   def destroy
