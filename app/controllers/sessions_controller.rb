7ba2b63e33d5d2212dbd818fdfa155f3431be744
Gina Son <ginasunah33@gmail.com>
2017-08-06 23:45:51 -0400

    create current user method and session form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    u = User.find_by(email: params[:session][:email])
+    u = User.find_by(email: params[:email])
 
-    if u && u.authenticate(params[:session][:password])
+    if u && u.authenticate(params[:password])
 
       session[:user_id] = u.id
 
       flash[:notice] = 'Sucessfully logged in!'
-      redirect_to root_url
+      redirect_to root_url, notice: "Logged in!"
     else
       flash.now[:alert] = 'Try again!'
       render :new
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:notice] = 'youre logged out!'
-    redirect_to root_url
+    redirect_to root_url, notice: "Logged out!"
   end
 
 end
